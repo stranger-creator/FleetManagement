1cd81d297ef05ca96433907c371a0c55cf38cff5
Shannon Pereira <shannonpereira971@gmail.com>
2023-10-18 10:40:08 +0530

    done

--- fleet/src/Components/Navbar.js
+++ fleet/src/Components/Navbar.js
@@ -1,28 +1,96 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import './Navbar.css'; // Import the CSS file
 
 const Navbar = () => {
+  const [menuVisible, setMenuVisible] = useState(false);
+  const [activeDropdown, setActiveDropdown] = useState(null);
+
+  const handleToggleMenu = () => {
+    setMenuVisible(!menuVisible);
+  };
+
+  const handleDropdownClick = (dropdownName) => {
+    if (activeDropdown === dropdownName) {
+      setActiveDropdown(null); // Close the currently open dropdown
+    } else {
+      setActiveDropdown(dropdownName); // Open the clicked dropdown
+    }
+  };
+
   return (
-    <nav className="navbar">
-      <ul>
-        <li>
-          <Link to="/dashboard">Dashboard</Link>
+    <div className="navbar">
+      <div className="toggle-button" onClick={handleToggleMenu}>
+        <i className="fas fa-bars"></i>
+      </div>
+      <ul className={`menu ${menuVisible ? 'active' : ''}`}>
+        <li onClick={() => handleDropdownClick('dashboard')}>
+          Dashboard
+          {activeDropdown === 'dashboard' && (
+            <ul className="dropdown">
+              <li>
+                <Link to="/dashboard/subitem1">Subitem 1</Link>
+              </li>
+              <li>
+                <Link to="/dashboard/subitem2">Subitem 2</Link>
+              </li>
+            </ul>
+          )}
         </li>
-        <li>
-          <Link to="/live-tracking">Live Tracking</Link>
+        <li onClick={() => handleDropdownClick('liveTracking')}>
+          Live Tracking
+          {activeDropdown === 'liveTracking' && (
+            <ul className="dropdown">
+              <li>
+                <Link to="/live-tracking/subitem1">Subitem 1</Link>
+              </li>
+              <li>
+                <Link to="/live-tracking/subitem2">Subitem 2</Link>
+              </li>
+            </ul>
+          )}
         </li>
-        <li>
-          <Link to="/schedule">Schedule</Link>
+        <li onClick={() => handleDropdownClick('schedule')}>
+          Schedule
+          {activeDropdown === 'schedule' && (
+            <ul className="dropdown">
+              <li>
+                <Link to="/schedule/subitem1">Subitem 1</Link>
+              </li>
+              <li>
+                <Link to="/schedule/subitem2">Subitem 2</Link>
+              </li>
+            </ul>
+          )}
         </li>
-        <li>
-          <Link to="/fleet">Fleet</Link>
+        <li onClick={() => handleDropdownClick('fleet')}>
+          Fleet
+          {activeDropdown === 'fleet' && (
+            <ul className="dropdown">
+              <li>
+                <Link to="/fleet/subitem1">Subitem 1</Link>
+              </li>
+              <li>
+                <Link to="/fleet/subitem2">Subitem 2</Link>
+              </li>
+            </ul>
+          )}
         </li>
-        <li>
-          <Link to="/services">Services</Link>
+        <li onClick={() => handleDropdownClick('services')}>
+          Services
+          {activeDropdown === 'services' && (
+            <ul className="dropdown">
+              <li>
+                <Link to="/services/subitem1">Subitem 1</Link>
+              </li>
+              <li>
+                <Link to="/services/subitem2">Subitem 2</Link>
+              </li>
+            </ul>
+          )}
         </li>
       </ul>
-    </nav>
+    </div>
   );
 };
 
