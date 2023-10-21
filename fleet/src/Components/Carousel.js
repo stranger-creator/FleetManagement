9b480ca8b21554c98cb1b60fb6f8c109602d6cc6
Shannon Pereira <shannonpereira971@gmail.com>
2023-10-21 19:22:00 +0530

    done

--- fleet/src/Components/Carousel.js
+++ fleet/src/Components/Carousel.js
@@ -22,24 +22,25 @@ export default function Carousel() {
       <div className="carousel-inner">
         <div className="carousel-item active" data-bs-interval="3000">
           <img
-            src="https://r4.wallpaperflare.com/wallpaper/704/217/93/future-bus-computer-desktop-background-wallpaper-025182607de6de4b2af8c215d028c962.jpg"
-            className="d-block w-100 carousel-image" // Add a class for the carousel images
-            alt="Image not found error 404"
-            style={imageStyle} // Apply the image style
-          />
-          <div className="carousel-caption d-none d-md-block">
-            <h1>welcome</h1>
-          </div>
-        </div>
-        <div className="carousel-item" data-bs-interval="3000">
-          <img
             src="https://t3.ftcdn.net/jpg/03/18/66/58/360_F_318665813_dASvsm41OsFeJZqkh0u2InH9P6ayS8Cw.jpg"
             className="d-block w-100 carousel-image" // Add a class for the carousel images
             alt="Image not found error 404"
             style={imageStyle} // Apply the image style
           />
           <div className="carousel-caption d-none d-md-block">
-            <h1>Optimizing Your Bus Routes.</h1>
+            <h1>Welcome</h1>
+          </div>
+        </div>
+        <div className="carousel-item" data-bs-interval="3000">
+          <img
+            src="https://ik.imagekit.io/fx/next/png/with-fleetx.png"
+            className="d-block w-100 carousel-image" // Add a class for the carousel images
+            alt="Image not found error 404"
+            style={imageStyle} // Apply the image style
+          />
+          <div className="carousel-caption d-none d-md-block">
+            <br></br>
+            <h1 style={{color:'black'}}>Optimizing Your Bus Routes</h1>
           </div>
         </div>
         <div className="carousel-item" data-bs-interval="3000">
