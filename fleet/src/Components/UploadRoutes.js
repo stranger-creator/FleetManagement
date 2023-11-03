00f91d13453927bb25568c41eb3a40adb75a4556
stranger-creator <raosuhas0@gmail.com>
2023-11-03 21:31:57 +0530

    notification

--- fleet/src/Components/UploadRoutes.js
+++ fleet/src/Components/UploadRoutes.js
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
 import axios from 'axios';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import './UserDataForm.css';
 
 const UserDataForm = () => {
@@ -27,66 +29,78 @@ const UserDataForm = () => {
       .then((response) => {
         // Handle the response from the backend, if needed
         console.log('Data sent successfully');
+
+        // Show a success toast
+        toast.success('Data submitted successfully', {
+          position: toast.POSITION.TOP_RIGHT,
+          autoClose: 3000, // Optional: Close the toast after 3 seconds
+        });
       })
       .catch((error) => {
         console.error('Error sending data:', error);
+
+        // Show an error toast
+        toast.error('Error sending data', {
+          position: toast.POSITION.TOP_RIGHT,
+          autoClose: 3000,
+        });
       });
   };
 
   return (
     <div className="user-data-form-container">
-  <div className="user-data-form-content">
-    <h2>Enter User Data</h2>
-    <form onSubmit={handleSubmit}>
-      <div className="input-group">
-        <label htmlFor="start">Start Location:</label>
-        <input
-          type="text"
-          id="start"
-          name="start"
-          value={formData.start}
-          onChange={handleChange}
-          required
-        />
+      <div className="user-data-form-content">
+        <h2>Enter User Data</h2>
+        <form onSubmit={handleSubmit}>
+          <div className="input-group">
+            <label htmlFor="start">Start Location:</label>
+            <input
+              type="text"
+              id="start"
+              name="start"
+              value={formData.start}
+              onChange={handleChange}
+              required
+            />
+          </div>
+          <div className="input-group">
+            <label htmlFor="end">End Location:</label>
+            <input
+              type="text"
+              id="end"
+              name="end"
+              value={formData.end}
+              onChange={handleChange}
+              required
+            />
+          </div>
+          <div className="input-group">
+            <label htmlFor="startTime">Start Time:</label>
+            <input
+              type="datetime-local"
+              id="startTime"
+              name="startTime"
+              value={formData.startTime}
+              onChange={handleChange}
+              required
+            />
+          </div>
+          <div className="input-group">
+            <label htmlFor="endTime">End Time:</label>
+            <input
+              type="datetime-local"
+              id="endTime"
+              name="endTime"
+              value={formData.endTime}
+              onChange={handleChange}
+              required
+            />
+          </div>
+          <button type="submit">Submit</button>
+        </form>
       </div>
-      <div className="input-group">
-        <label htmlFor="end">End Location:</label>
-        <input
-          type="text"
-          id="end"
-          name="end"
-          value={formData.end}
-          onChange={handleChange}
-          required
-        />
-      </div>
-      <div className="input-group">
-        <label htmlFor="startTime">Start Time:</label>
-        <input
-          type="datetime-local"
-          id="startTime"
-          name="startTime"
-          value={formData.startTime}
-          onChange={handleChange}
-          required
-        />
-      </div>
-      <div className="input-group">
-        <label htmlFor="endTime">End Time:</label>
-        <input
-          type="datetime-local"
-          id="endTime"
-          name="endTime"
-          value={formData.endTime}
-          onChange={handleChange}
-          required
-        />
-      </div>
-      <button type="submit">Submit</button>
-    </form>
-  </div>
-</div>
-
+      <ToastContainer />
+    </div>
   );
 };
 
