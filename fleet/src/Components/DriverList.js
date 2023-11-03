d27787cc85724f269db6a97321e6c413eb60c4ef
shannon <shannonpereira971@gmail.com>
2023-11-03 12:10:11 +0530

    done

--- fleet/src/Components/DriverList.js
+++ fleet/src/Components/DriverList.js
@@ -1,4 +1,5 @@
-import React from 'react';
+
+import React,{useState} from 'react';
 import './DriverList.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBus } from '@fortawesome/free-solid-svg-icons';
@@ -20,69 +21,149 @@ const drivers = [
   },
   {
     id: 3,
-    name: 'Michael Johnson',
-    route: 'Route 303',
-    dutyTiming: '9:15 AM - 5:15 PM',
-    vehicleNumber: 'Bus #C789',
+    name: 'Hardman',
+    route: 'Route 456',
+    dutyTiming: '5:00 AM - 12:30 PM',
+    vehicleNumber: 'Bus #C157',
   },
   {
     id: 4,
-    name: 'Emily Wilson',
-    route: 'Route 404',
-    dutyTiming: '6:45 AM - 2:45 PM',
-    vehicleNumber: 'Bus #D012',
+    name: 'Emily Specter',
+    route: 'Route 678',
+    dutyTiming: '7:30 AM - 3:30 PM',
+    vehicleNumber: 'Bus #D358',
   },
   {
     id: 5,
-    name: 'Daniel Brown',
-    route: 'Route 505',
-    dutyTiming: '10:30 AM - 6:30 PM',
-    vehicleNumber: 'Bus #E345',
+    name: 'Austin',
+    route: 'Route 789',
+    dutyTiming: '9:30 AM - 12:30 PM',
+    vehicleNumber: 'Bus #E116',
   },
   {
     id: 6,
-    name: 'Olivia Lee',
-    route: 'Route 606',
-    dutyTiming: '7:00 AM - 3:00 PM',
-    vehicleNumber: 'Bus #F678',
+    name: 'Alwyn',
+    route: 'Route 475',
+    dutyTiming: '8:30 AM - 3:30 PM',
+    vehicleNumber: 'Bus #F974',
   },
   {
     id: 7,
-    name: 'William Davis',
-    route: 'Route 707',
-    dutyTiming: '8:45 AM - 4:45 PM',
-    vehicleNumber: 'Bus #G901',
+    name: 'Micheal',
+    route: 'Route 211',
+    dutyTiming: '6:30 AM - 1:30 PM',
+    vehicleNumber: 'Bus #G456',
   },
   {
     id: 8,
-    name: 'Ava Rodriguez',
-    route: 'Route 808',
-    dutyTiming: '11:15 AM - 7:15 PM',
-    vehicleNumber: 'Bus #H234',
+    name: 'Sam',
+    route: 'Route 967',
+    dutyTiming: '7:30 AM - 3:30 PM',
+    vehicleNumber: 'Bus #H456',
   },
   {
     id: 9,
-    name: 'Timothy Mclain',
-    route: 'Route 909',
-    dutyTiming: '12:25 AM - 5:30 PM',
-    vehicleNumber: 'Bus #I123',
+    name: 'Pheonix',
+    route: 'Route 444',
+    dutyTiming: '6:50 AM - 4:45 PM',
+    vehicleNumber: 'Bus #I456',
   },
-];
+  {
+    id: 10,
+    name: 'Fredrick',
+    route: 'Route 202',
+    dutyTiming: '7:30 AM - 3:30 PM',
+    vehicleNumber: 'Bus #J456',
+  },
+  {
+    id: 11,
+    name: 'Aldino',
+    route: 'Route 202',
+    dutyTiming: '7:30 AM - 3:30 PM',
+    vehicleNumber: 'Bus #K456',
+  },
+  {
+    id: 12,
+    name: 'Jaden Ross',
+    route: 'Route 120',
+    dutyTiming: '7:45 AM - 1:30 PM',
+    vehicleNumber: 'Bus #L236',
+  },
+  {
+    id: 13,
+    name: 'Wilston',
+    route: 'Route 505',
+    dutyTiming: '1:30 PM - 6:30 PM',
+    vehicleNumber: 'Bus #W456',
+  },
+  {
+    id: 14,
+    name: 'Rhea John',
+    route: 'Route 140',
+    dutyTiming: '6:30 AM - 2:30 PM',
+    vehicleNumber: 'Bus #N456',
+  },
 
+  // Add more driver objects here
+  {
+    id: 15,
+    name: 'Ella Johnson',
+    route: 'Route 505',
+    dutyTiming: '10:30 AM - 6:30 PM',
+    vehicleNumber: 'Bus #M789',
+  },
+  {
+    id: 16,
+    name: 'David White',
+    route: 'Route 606',
+    dutyTiming: '7:00 AM - 3:00 PM',
+    vehicleNumber: 'Bus #P678',
+  },
+  // Add as many driver objects as needed
+];
 const DriverList = () => {
+  const [searchTerm, setSearchTerm] = useState('');
+
+  const filteredDrivers = drivers.filter(
+    (driver) =>
+      driver.name.toLowerCase().includes(searchTerm.toLowerCase()) ||
+      driver.route.toLowerCase().includes(searchTerm.toLowerCase()) ||
+      driver.dutyTiming.toLowerCase().includes(searchTerm.toLowerCase()) ||
+      driver.vehicleNumber.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
   return (
-    <div className="driverList">
-      {drivers.map((driver) => (
-        <div key={driver.id} className="driverCard">
-          <h2>{driver.name}</h2>
-          <p>Route: {driver.route}</p>
-          <p>Duty Timing: {driver.dutyTiming}</p>
-          <p>
-            <FontAwesomeIcon icon={faBus} />
-            {driver.vehicleNumber}
-          </p>
-        </div>
-      ))}
+    <div className="driverListContainer">
+      <input
+        type="text"
+        placeholder="Search by name, route, duty timing, or vehicle number"
+        value={searchTerm}
+        onChange={(e) => setSearchTerm(e.target.value)}
+        className="searchBar"
+      />
+      <table className="driverList">
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Route</th>
+            <th>Duty Timing</th>
+            <th>Vehicle Number</th>
+          </tr>
+        </thead>
+        <tbody>
+          {filteredDrivers.map((driver) => (
+            <tr key={driver.id} className="driverRow">
+              <td>{driver.name}</td>
+              <td>{driver.route}</td>
+              <td>{driver.dutyTiming}</td>
+              <td>
+                <FontAwesomeIcon icon={faBus} />
+                {driver.vehicleNumber}
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 };
--- fleet/src/Components/UploadRoutes.js
+++ fleet/src/Components/UploadRoutes.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import axios from 'axios';
-
+import './UserDataForm.css';
 const UserDataForm = () => {
   const [formData, setFormData] = useState({
     start: '',
